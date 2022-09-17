062e707bd23c45d355f7fa03d0b638a2678228d7
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2022-09-17 20:57:26 -0300

    feat: creating html with js and using LocalStorage

--- js/Favorites.js
+++ js/Favorites.js
@@ -3,7 +3,22 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
     } 
+
+    load() {
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+
+    
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter((entry) => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
+    }
+
 }
 
 
@@ -11,20 +26,61 @@ export class Favorites {
 export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
-
+        this.tbody = this.root.querySelector('table tbody')
         this.update()
     }
 
     update() {
         this.removeAllTr()
-
         
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de $user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+    }
+    
+    createRow() {
+        const tr = document.createElement('tr')
+        
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/calebefelipe.png" alt="Imagem de CalebeFelipe">
+                <a href="https://github.com/calebefelipe" target="_blank">
+                    <p>Calebe Felipe</p>
+                    <span>calebefelipe</span>
+                </a>
+            </td>
+            <td class="repositories">24</td>
+            <td class="followers">7</td>
+            <td>
+                <button class="remove">&times;</button>
+            </td>
+        `
+
+        return tr
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
+        
 
-        tbody.querySelectorAll('tr').forEach((tr) => {
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         })
     }
